ce2a28a4c7d7fb97dfbfdeea977e6877452c521f
Paulo Turino <paulo.turino01@gmail.com>
2023-11-12 08:14:15 -0300

    fix: Fix bug when adding same card twice

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { GiPunch, GiBrain, GiCheckedShield, GiCometSpark, GiBattleGear } from "react-icons/gi";
+import {
+  GiPunch,
+  GiBrain,
+  GiCheckedShield,
+  GiCometSpark,
+  GiBattleGear,
+} from "react-icons/gi";
 import { AiFillThunderbolt } from "react-icons/ai";
 import { useContext } from "react";
 import { ContextAPI } from "@/context/ContextAPI";
@@ -7,17 +13,23 @@ import { ContextAPI } from "@/context/ContextAPI";
 type CardProps = {
   id: number;
   imgs: {
-    sm: string,
-    md: string,
+    sm: string;
+    md: string;
   };
   name: string;
   powerstats: {
     [key: string]: number;
   };
-  sm: boolean; 
+  sm: boolean;
 };
 
-export default function HeroCard({ name, imgs, powerstats, id, sm }: CardProps) {
+export default function HeroCard({
+  name,
+  imgs,
+  powerstats,
+  id,
+  sm,
+}: CardProps) {
   const { cardsSelection, setCardsSelection } = useContext(ContextAPI);
 
   const totalPower = Object.values(powerstats).reduce(
@@ -37,30 +49,42 @@ export default function HeroCard({ name, imgs, powerstats, id, sm }: CardProps) 
 
   const handleClick = (sm: boolean) => {
     if (cardsSelection.length < 2 && !sm) {
-      const selectedCard = { name, imgs, powerstats, id, totalPower };
-      setCardsSelection([...cardsSelection, selectedCard]);
+      const isIdAlreadySelected = cardsSelection.some((card) => card.id === id);
+
+      if (!isIdAlreadySelected) {
+        const selectedCard = { name, imgs, powerstats, id, totalPower };
+        setCardsSelection([...cardsSelection, selectedCard]);
+      }
     }
   };
 
   return (
-    <div onClick={() => handleClick(sm)} className={`rounded-lg border-2 border-[var(--${maxPowerstatName})] overflow-hidden flex flex-col items-center ${!sm && `cursor-pointer hover:scale-105 hover:shadow-[4px_4px_20px_6px_var(--${maxPowerstatName})] transition duration-500`}`}>
+    <div
+      onClick={() => handleClick(sm)}
+      className={`rounded-lg border-2 border-[var(--${maxPowerstatName})] overflow-hidden flex flex-col items-center ${
+        !sm &&
+        `cursor-pointer hover:scale-105 hover:shadow-[4px_4px_20px_6px_var(--${maxPowerstatName})] transition duration-500`
+      }`}
+    >
       <Image src={imgs.md} alt={name} width={150} height={150} />
       <div className="pt-2 ">
-        {!sm && <p className="flex justify-center items-center gap-2 text-lg">
-          {maxPowerstatName === "strength" ? (
-            <GiPunch />
-          ) : maxPowerstatName === "intelligence" ? (
-            <GiBrain />
-          ) : maxPowerstatName === "speed" ? (
-            <AiFillThunderbolt />
-          ) : maxPowerstatName === "durability" ? (
-            <GiCheckedShield />
-          ) :  maxPowerstatName === "power" ? (
-            <GiCometSpark />
-          ) : (
-            <GiBattleGear />
-          )}
-        </p>}
+        {!sm && (
+          <p className="flex justify-center items-center gap-2 text-lg">
+            {maxPowerstatName === "strength" ? (
+              <GiPunch />
+            ) : maxPowerstatName === "intelligence" ? (
+              <GiBrain />
+            ) : maxPowerstatName === "speed" ? (
+              <AiFillThunderbolt />
+            ) : maxPowerstatName === "durability" ? (
+              <GiCheckedShield />
+            ) : maxPowerstatName === "power" ? (
+              <GiCometSpark />
+            ) : (
+              <GiBattleGear />
+            )}
+          </p>
+        )}
         <h3 className="text-lg font-bold text-center w-full">{name}</h3>
       </div>
     </div>
--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -1,4 +1,4 @@
-import { TbTriangleFilled, TbTriangleInvertedFilled } from "react-icons/tb";
+import { TbTriangleFilled, TbTriangleInvertedFilled, TbEqual } from "react-icons/tb";
 
 interface TableRowProps {
   statName: string;
@@ -18,11 +18,13 @@ export default function TableRow({
           <span className="flex items-center gap-2 text-[var(--winner)]">
             {statHero1}<TbTriangleFilled />
           </span>
-        ) : (
+        ) : statHero1 < statHero2 ? (
           <span className="flex items-center gap-2 text-[var(--looser)]">
             {statHero1}<TbTriangleInvertedFilled />
           </span>
-        )}
+        ): <span className="flex items-center gap-2 text-[var(--light-gray)]">
+          {statHero1}<TbEqual />
+        </span>}
       </td>
       <td
         className="font-bold capitalize w-[200px] text-center"
@@ -34,11 +36,14 @@ export default function TableRow({
           <span className="flex items-center gap-2 text-[var(--winner)]">
             <TbTriangleFilled />{statHero2}
           </span>
-        ) : (
+        ) : statHero2 < statHero1 ? (
           <span className="flex items-center gap-2 text-[var(--looser)]">
             <TbTriangleInvertedFilled />{statHero2}
           </span>
-        )}
+        ) : <span className="flex items-center gap-2 text-[var(--light-gray)]">
+          <TbEqual />{statHero2}
+        </span>
+        }
       </td>
     </tr>
   );
